cf1e3f1eb5117ea685fcec24e6c0d173ef9a0e69
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-07-14 12:29:24 -0400

    update search indexing function

--- app-services/functions/writePageContents.js
+++ app-services/functions/writePageContents.js
@@ -1,39 +1,46 @@
-exports = function ({ fullDocument }) {
+function getDocsFromHtml(htmlStr){
+  const { NodeHtmlMarkdown } = require("node-html-markdown");
+  const removeMd = require('remove-markdown');
+
+  const md = NodeHtmlMarkdown.translate(htmlStr);
+  const plainText = removeMd(md);
+  return plainText;
+}
+
+exports = async function({ fullDocument }) {
   const axios = require("axios");
+  
 
   //for testing
-  if (!fullDocument) {
+  if(!fullDocument){
     fullDocument = {
-      loc: "https://www.mongodb.com/docs/realm/sdk/java/quick-start-local/",
-    };
+      loc: "https://www.mongodb.com/docs/realm/sdk/flutter/realm-database/"  
+    }
+  }
+  
+  const pageUrl = fullDocument.loc;
+  
+  const {data: html } = await axios.get(pageUrl);
+  const docText = getDocsFromHtml(html);
+
+  
+  const titleRegex = /<title.*>(.*)<\/title>/;
+  const pageTitleRes = titleRegex.exec(html);
+  let title;
+  if(pageTitleRes !== null){
+    title = pageTitleRes[1];
   }
 
-  const pageUrl = fullDocument.loc;
-
-  axios.get(pageUrl).then((res) => {
-    const html = res.data;
-    const titleRegex = /<title.*>(.*)<\/title>/;
-    const pageTitleRes = titleRegex.exec(html);
-    let title;
-    if (pageTitleRes !== null) {
-      title = pageTitleRes[1];
-    }
-    const docText = html.replace(/<[^>]*>?/gm, "");
-
-    // update search index collection
-    const pageContents = context.services
-      .get("mongodb-atlas")
-      .db("site-search")
-      .collection("page-contents");
-    const query = { _id: pageUrl };
-    const updateDoc = {
-      $set: {
-        title,
-        doc_text: docText,
-      },
-      $currentDate: { last_updated: true },
-    };
-    const options = { upsert: true };
-    pageContents.updateOne(query, updateDoc, options);
-  });
+  // update search index collection
+  const pageContents = context.services.get("mongodb-atlas").db("site-search").collection("page-contents");
+  const query = { _id: pageUrl };
+  const updateDoc = {
+    $set: {
+      title,
+      doc_text: docText,
+    },
+    $currentDate: { last_updated: true }
+  };
+  const options = { upsert: true };
+  pageContents.updateOne(query, updateDoc, options);
 };
