3de35d31f8c1efd4536810a046b8eb76767df6dc
Marcus <marcuseagan@gmail.com>
2022-07-19 12:05:48 -0700

    Update scripts/create-atlas-search-index.sh
    add this one
    
    Co-authored-by: Ben Perlmutter <90647379+mongodben@users.noreply.github.com>

--- scripts/create-atlas-search-index.sh
+++ scripts/create-atlas-search-index.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #TODO replace the group name and cluster name so this actually works
-curl --user "{service account public key}:{service account private key}" --digest \
+curl --user "${PUBLIC_KEY}:${PRIVATE_KEY}" --digest \
      --header "Content-Type: application/json" \
      --include \
      --request POST "https://cloud.mongodb.com/api/atlas/v1.0/groups/${GROUP_ID}/clusters/${PROJECT_NAME}/fts/indexes?pretty=true" \
