5dec5dbcc7351ac08e9b85fcc0ddb22c17fcdaf7
Marcus <marcuseagan@gmail.com>
2022-07-19 13:37:18 -0700

    Update scripts/create-atlas-search-index.sh
    thanks again on cluster name
    
    Co-authored-by: Ben Perlmutter <90647379+mongodben@users.noreply.github.com>

--- scripts/create-atlas-search-index.sh
+++ scripts/create-atlas-search-index.sh
@@ -4,5 +4,5 @@
 curl --user "${PUBLIC_KEY}:${PRIVATE_KEY}" --digest \
      --header "Content-Type: application/json" \
      --include \
-     --request POST "https://cloud.mongodb.com/api/atlas/v1.0/groups/${GROUP_ID}/clusters/${PROJECT_NAME}/fts/indexes?pretty=true" \
+     --request POST "https://cloud.mongodb.com/api/atlas/v1.0/groups/${GROUP_ID}/clusters/${CLUSTER_NAME}/fts/indexes?pretty=true" \
      --data-binary @$1
