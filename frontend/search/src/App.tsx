3db3acfcd8e2e2308da868f83c11197589b6c8d4
Ben Perlmutter <90647379+mongodben@users.noreply.github.com>
2022-07-22 11:30:44 -0400

    Polish demo site (#61)
    update demo site

--- frontend/search/src/App.tsx
+++ frontend/search/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "./App.css";
 import SearchPage from "./pages/SearchPage";
 import { APP_ID } from "./constants";
+import { BottomNavigation, Typography, Link } from "@mui/material";
 
 function App() {
   return (
@@ -12,6 +12,26 @@ function App() {
       }}
     >
       <SearchPage id={APP_ID} />
+      <BottomNavigation
+        style={{
+          position: "absolute",
+          bottom: 0,
+          width: "100%",
+          display: "flex",
+          justifyContent: "center",
+          padding: "0 20px",
+          flexDirection: "row",
+        }}
+      >
+        <Typography variant="body1" align="center">
+          For more information on Atlas Static Site Search and how to add it to
+          your website, refer to{" "}
+          <Link href="https://github.com/mongodben/atlas-static-site-search/issues">
+            the project README on Github
+          </Link>
+          .
+        </Typography>
+      </BottomNavigation>
     </div>
   );
 }
--- frontend/search/src/pages/SearchPage.tsx
+++ frontend/search/src/pages/SearchPage.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 import Search from "atlas-static-site-search-box";
+import QueryIcon from "./query-icon.png";
+import BottomNavigation from "@mui/material/BottomNavigation";
 
 interface SearchProps {
   id: string;
@@ -17,7 +20,16 @@ export default function SearchPage({ id }: SearchProps) {
         height: "400px",
       }}
     >
-      <Typography variant="h3">🥭</Typography>
+      <Typography variant="h3">
+        <img src={QueryIcon} alt="search query icon" style={{ height: 50 }} />
+      </Typography>
+      <Typography variant="h3" variantMapping={{ h3: "h1" }}>
+        Atlas Static Site Search
+      </Typography>
+      <Typography variant="subtitle2">
+        Demo using Atlas Static Site Search to query the{" "}
+        <Link href="https://mongodb.com/docs/realm">Realm documentation</Link>
+      </Typography>
       <Search id={id} />
     </div>
   );
