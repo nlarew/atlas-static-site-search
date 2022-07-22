46ccd03b7785ce57aa2f78661d39e3d86324dea5
Nick Larew <nick.larew@mongodb.com>
2022-07-22 14:22:51 -0400

    quiet curl in search index script output

--- scripts/create-atlas-search-index.sh
+++ scripts/create-atlas-search-index.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl --user "${PUBLIC_KEY}:${PRIVATE_KEY}" --digest \
+curl -s --user "${PUBLIC_KEY}:${PRIVATE_KEY}" --digest \
      --header "Content-Type: application/json" \
      --include \
      --request POST "https://cloud.mongodb.com/api/atlas/v1.0/groups/${GROUP_ID}/clusters/${CLUSTER_NAME}/fts/indexes?pretty=true" \
